7dfe924b8f3a0769ba70528b722f70eb09fc3152
Joshua Parker <joshua@joshuaparker.dev>
2026-04-17 19:47:04 -0700

    Moved controller from getdevflow/cmf so that it does not get edited.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

---
+++ Infrastructure/Http/Controllers/CronController.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Http\Controllers;
+
+use Codefy\CommandBus\Exceptions\CommandPropertyNotFoundException;
+use Codefy\CommandBus\Exceptions\UnresolvableCommandHandlerException;
+use Codefy\Framework\Http\BaseController;
+use Codefy\QueryBus\UnresolvableQueryHandlerException;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\SimpleCache\InvalidArgumentException;
+use Qubus\Exception\Data\TypeException;
+use Qubus\Exception\Exception;
+use Qubus\Http\ServerRequest;
+use ReflectionException;
+
+use function App\Shared\Helpers\get_all_sites;
+use function App\Shared\Helpers\publish_scheduled_content;
+use function App\Shared\Helpers\publish_scheduled_product;
+use function App\Shared\Helpers\restore_current_site;
+use function App\Shared\Helpers\switch_to_site;
+
+final class CronController extends BaseController
+{
+    /**
+     * @param ServerRequest $request
+     * @return void
+     * @throws CommandPropertyNotFoundException
+     * @throws ContainerExceptionInterface
+     * @throws Exception
+     * @throws InvalidArgumentException
+     * @throws NotFoundExceptionInterface
+     * @throws ReflectionException
+     * @throws TypeException
+     * @throws UnresolvableCommandHandlerException
+     * @throws UnresolvableQueryHandlerException
+     */
+    public function master(ServerRequest $request): void
+    {
+        foreach (get_all_sites() as $site) {
+            switch_to_site($site['key']);
+            publish_scheduled_content();
+            publish_scheduled_product();
+            restore_current_site();
+        }
+    }
+}
