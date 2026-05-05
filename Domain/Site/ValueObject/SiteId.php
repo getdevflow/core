7a46324bd8200deafe982dd7cc49e7e1e62f7d01
Joshua Parker <joshua@joshuaparker.dev>
2026-05-05 06:40:24 -0600

    Removed DDD specific code.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- Domain/Site/ValueObject/SiteId.php
+++ Domain/Site/ValueObject/SiteId.php
@@ -4,18 +4,11 @@ declare(strict_types=1);
 
 namespace App\Domain\Site\ValueObject;
 
-use App\Domain\Site\Site;
-use Codefy\Domain\Aggregate\AggregateId;
 use Qubus\Exception\Data\TypeException;
 use Qubus\ValueObjects\Identity\Ulid;
 
-class SiteId extends Ulid implements AggregateId
+class SiteId extends Ulid
 {
-    public function aggregateClassName(): string
-    {
-        return Site::className();
-    }
-
     /**
      * @throws TypeException
      */
--- Domain/User/ValueObject/UserId.php
+++ Domain/User/ValueObject/UserId.php
@@ -4,18 +4,11 @@ declare(strict_types=1);
 
 namespace App\Domain\User\ValueObject;
 
-use App\Domain\User\User;
-use Codefy\Domain\Aggregate\AggregateId;
 use Qubus\Exception\Data\TypeException;
 use Qubus\ValueObjects\Identity\Ulid;
 
-class UserId extends Ulid implements AggregateId
+class UserId extends Ulid
 {
-    public function aggregateClassName(): string
-    {
-        return User::className();
-    }
-
     /**
      * @throws TypeException
      */
