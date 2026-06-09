bbea5130798e629eedf3176b0d53ea05b3b9912f
Joshua Parker <joshua@joshuaparker.dev>
2026-06-09 10:00:58 -0600

    Fixed for use by pagebuilder manager instead of using the default one.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- Infrastructure/Http/Controllers/WebsiteManagerController.php
+++ Infrastructure/Http/Controllers/WebsiteManagerController.php
@@ -19,7 +19,6 @@ use ReflectionException;
 use Vihzhuo\Contracts\PageContract;
 use Vihzhuo\Repositories\PageRepository;
 
-use function App\Shared\Helpers\add_page_attribute;
 use function App\Shared\Helpers\admin_url;
 use function App\Shared\Helpers\current_user_can;
 use function App\Shared\Helpers\update_page_attribute;
@@ -27,7 +26,7 @@ use function Codefy\Framework\Helpers\config;
 use function Codefy\Framework\Helpers\trans;
 use function Codefy\Framework\Helpers\view;
 use function phpb_trans;
-use function phpb_url;
+use function sprintf;
 
 final class WebsiteManagerController extends BaseController
 {
@@ -45,7 +44,7 @@ final class WebsiteManagerController extends BaseController
      * @throws ReflectionException
      * @throws \Exception
      */
-    public function index(ServerRequest $request): ResponseInterface
+    public function index(): ResponseInterface
     {
         if (false === current_user_can(perm: 'vihzhuo:manage')) {
             Devflow::$PHP->flash->error(
@@ -60,26 +59,6 @@ final class WebsiteManagerController extends BaseController
         $pageRepository = new PageRepository();
         $pages = $pageRepository->getAll();
 
-        if (isset($request->getQueryParams()['route']) && $request->getQueryParams()['route'] === 'page_settings') {
-            if ($request->getQueryParams()['action'] === 'create') {
-                return $this->handleCreate($request);
-            }
-
-            /** @var string $pageId */
-            $pageId = $request->getQueryParams()['page'] ?? null;
-            $pageRepository = new PageRepository();
-            $page = $pageRepository->findWithId($pageId);
-            if (! ($page instanceof PageContract)) {
-                return $this->redirect(phpb_url('website_manager'));
-            }
-
-            if ($request->getQueryParams()['action'] === 'edit') {
-                return $this->handleEdit($page, $request);
-            } elseif ($request->getQueryParams()['action'] === 'destroy') {
-                return $this->handleDestroy($page);
-            }
-        }
-
         return view(
             template: $this->managerIndexTemplate,
             data: [
@@ -87,6 +66,135 @@ final class WebsiteManagerController extends BaseController
                 'pages' => $pages,
             ]
         );
+    }
+
+    /**
+     * @param ServerRequest $request
+     * @return ResponseInterface
+     * @throws ContainerExceptionInterface
+     * @throws Exception
+     * @throws InvalidArgumentException
+     * @throws NotFoundExceptionInterface
+     * @throws ReflectionException
+     * @throws TypeException
+     * @throws \Exception
+     */
+    public function create(ServerRequest $request): ResponseInterface
+    {
+        if (false === current_user_can(perm: 'vihzhuo:manage')) {
+            Devflow::$PHP->flash->error(message: trans('Access denied.'));
+            return $this->redirect(admin_url());
+        }
+
+        $this->vihzhuoInstance();
+
+        if ($request->getMethod() === 'POST') {
+            $body = (array) $request->getParsedBody();
+
+            $pageRepository = new PageRepository();
+            $page = $pageRepository->create($body);
+
+            if ($page) {
+                Devflow::$PHP->flash->success(
+                    phpb_trans(key: 'website-manager.page-created')
+                );
+
+                return $this->redirect(admin_url('manager/'));
+            }
+        }
+
+        return $this->renderPageSettings();
+    }
+
+    /**
+     * @param ServerRequest $request
+     * @param int $pageId
+     * @return ResponseInterface
+     * @throws ContainerExceptionInterface
+     * @throws Exception
+     * @throws InvalidArgumentException
+     * @throws NotFoundExceptionInterface
+     * @throws ReflectionException
+     * @throws TypeException
+     * @throws \Exception
+     */
+    public function edit(ServerRequest $request, int $pageId): ResponseInterface
+    {
+        if (false === current_user_can(perm: 'vihzhuo:manage')) {
+            Devflow::$PHP->flash->error(message: trans('Access denied.'));
+            return $this->redirect(admin_url());
+        }
+
+        $this->vihzhuoInstance();
+
+        $pageRepository = new PageRepository();
+        $page = $pageRepository->findWithId((string) $pageId);
+
+        if ($page === null) {
+            return $this->redirect(admin_url('manager/'));
+        }
+
+        if ($request->getMethod() === 'POST') {
+            $body = (array) $request->getParsedBody();
+
+            $success = $pageRepository->update($page, $body);
+
+            if ($success) {
+                if (isset($body['page_field']) && is_array($body['page_field'])) {
+                    foreach ($body['page_field'] as $key => $value) {
+                        update_page_attribute($page->getId(), (string) $key, $value);
+                    }
+                }
+
+                Action::getInstance()->doAction('update_page', $page);
+
+                Devflow::$PHP->flash->success(
+                    phpb_trans(key: 'website-manager.page-updated')
+                );
+
+                return $this->redirect(admin_url(sprintf('manager/%s/', $page->getId())));
+            }
+        }
+
+        return $this->renderPageSettings($page);
+    }
+
+    /**
+     * @param ServerRequest $request
+     * @param int $pageId
+     * @return ResponseInterface
+     * @throws ContainerExceptionInterface
+     * @throws Exception
+     * @throws InvalidArgumentException
+     * @throws NotFoundExceptionInterface
+     * @throws ReflectionException
+     * @throws TypeException
+     */
+    public function destroy(ServerRequest $request, int $pageId): ResponseInterface
+    {
+        if (false === current_user_can(perm: 'vihzhuo:manage')) {
+            Devflow::$PHP->flash->error(message: trans('Access denied.'));
+            return $this->redirect(admin_url());
+        }
+
+        $this->vihzhuoInstance();
+
+        $pageRepository = new PageRepository();
+        $page = $pageRepository->findWithId((string) $pageId);
+
+        if ($page === null) {
+            return $this->redirect(admin_url('manager/'));
+        }
+
+        $pageRepository->destroy($page->getId());
+
+        Action::getInstance()->doAction('deleted_page', $page);
+
+        Devflow::$PHP->flash->success(
+            phpb_trans(key: 'website-manager.page-deleted')
+        );
+
+        return $this->redirect(admin_url('manager/'));
     }
 
     /**
@@ -117,95 +225,5 @@ final class WebsiteManagerController extends BaseController
                 'theme' => $theme,
             ]
         );
-    }
-
-    /**
-     * @param ServerRequest $request
-     * @return ResponseInterface
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     * @throws ReflectionException
-     * @throws TypeException
-     * @throws \Exception
-     */
-    private function handleCreate(ServerRequest $request): ResponseInterface
-    {
-        if ($request->getMethod() === 'POST') {
-            $body = (array) $request->getParsedBody();
-            $pageRepository = new PageRepository();
-            $page = $pageRepository->create($body);
-            if ($page) {
-                if (isset($body['page_field'])) {
-                    foreach ($body['page_field'] as $key => $value) {
-                        add_page_attribute($page->getId(), $key, $value);
-                    }
-                }
-
-                Action::getInstance()->doAction('create_page', $page);
-
-                /** @var string $message */
-                $message = phpb_trans(key: 'website-manager.page-created');
-                Devflow::$PHP->flash->success($message);
-
-                return $this->redirect(phpb_url('website_manager'));
-            }
-        }
-
-        return $this->renderPageSettings();
-    }
-
-    /**
-     * @param PageContract $page
-     * @param ServerRequest $request
-     * @return ResponseInterface
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     * @throws ReflectionException
-     * @throws TypeException
-     * @throws \Exception
-     */
-    private function handleEdit(PageContract $page, ServerRequest $request): ResponseInterface
-    {
-        if ($request->getMethod() === 'POST') {
-            $body = (array) $request->getParsedBody();
-            $pageRepository = new PageRepository();
-            $success = $pageRepository->update($page, $body);
-            if ($success) {
-                if (isset($body['page_field'])) {
-                    foreach ($body['page_field'] as $key => $value) {
-                        update_page_attribute($page->getId(), $key, $value);
-                    }
-                }
-
-                Action::getInstance()->doAction('update_page', $page);
-
-                /** @var string $message */
-                $message = phpb_trans(key: 'website-manager.page-updated');
-                Devflow::$PHP->flash->success($message);
-
-                return $this->redirect(phpb_url(module: 'website_manager'));
-            }
-        }
-
-        return $this->renderPageSettings($page);
-    }
-
-    /**
-     * @param PageContract $page
-     * @return ResponseInterface
-     * @throws Exception
-     * @throws ReflectionException
-     */
-    private function handleDestroy(PageContract $page): ResponseInterface
-    {
-        $pageRepository = new PageRepository();
-        $pageRepository->destroy($page->getId());
-        /** @var string $message */
-        $message = phpb_trans(key: 'website-manager.page-deleted');
-        Devflow::$PHP->flash->success($message);
-
-        Action::getInstance()->doAction('deleted_page', $page);
-
-        return $this->redirect(phpb_url('website_manager'));
     }
 }
