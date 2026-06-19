5f47426f497f69135b86becc7aeaa7ab8a1df7dc
Joshua Parker <joshua@joshuaparker.dev>
2026-06-19 14:29:13 -0600

    Fix password check config.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- Infrastructure/Http/Controllers/AdminUserController.php
+++ Infrastructure/Http/Controllers/AdminUserController.php
@@ -95,11 +95,11 @@ final class AdminUserController extends BaseController
             return $this->redirect(admin_url());
         }
 
-        if (strlen($request->get('pass')) < config()->integer(key: 'cms.password_length')) {
+        if (strlen($request->get('pass')) < config()->integer(key: 'auth.password_length')) {
             Devflow::$PHP->flash->error(
                 message: sprintf(
                     trans(string: 'Passwords cannot be less than %s characters.'),
-                    config()->integer(key: 'cms.password_length')
+                    config()->integer(key: 'auth.password_length')
                 ),
             );
             $this->redirect($request->getHeaderLine(name: 'Referer'));
